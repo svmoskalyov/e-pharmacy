e20a77deac7a19254c53d6c65db461921c5e9529
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-10 12:13:06 +0200

    draft rating

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,28 @@
+import s from './Rating.module.scss'
+
+interface RatingProps {
+  initialRating: number
+}
+
+function Rating({ initialRating }: RatingProps) {
+  const query = 'mobile'
+  const stars = query === 'mobile' ? [1] : [1, 2, 3, 4, 5]
+
+  return (
+    <div className={s.rating}>
+      {stars.map(star => (
+        <span
+          key={star}
+          className={
+            initialRating >= star ? `${s.star} ${s.filled}` : `${s.star}`
+          }
+        >
+          ★
+        </span>
+      ))}
+      <span className={s.value}>{initialRating}</span>
+    </div>
+  )
+}
+
+export default Rating
