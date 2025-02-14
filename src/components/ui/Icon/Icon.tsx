581a5db207c99359715bd3ab37c74cb465e617b0
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-14 14:44:54 +0200

    Update Icon.tsx

--- src/components/ui/Icon/Icon.tsx
+++ src/components/ui/Icon/Icon.tsx
@@ -3,16 +3,17 @@ import sprite from '../../../assets/icons/sprite.svg'
 type IconProps = {
   name: string
   size: string
-  fill?: string
+  color?: string
 }
 
-function Icon({ name, size, fill='inherit' }: IconProps) {
+function Icon({ name, size, color = '#fff' }: IconProps) {
   return (
     <svg
       style={{
         width: `${size}`,
         height: `${size}`,
-        fill: `${fill}`,
+        fill: `${color}`,
+        stroke: `${color}`
       }}
     >
       <use href={sprite + `#${name}`} />
