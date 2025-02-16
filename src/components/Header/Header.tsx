d761d7660f47931e3ba24a6e7ca7733fdd0bd571
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-16 10:03:05 +0200

    create Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,48 @@
+import { useState } from 'react'
+import { useLocation } from 'react-router'
+import Logo from '../ui/Logo'
+import Icon from '../ui/Icon'
+import Drawer from '../Drawer'
+import NavigationLinks from '../NavigationLinks'
+import AuthenticationLinks from '../AuthenticationLinks'
 import s from './Header.module.scss'
 
 function Header() {
-  return <header className={s.header}>Header</header>
+  const [isOpen, setIsOpen] = useState(false)
+  const location = useLocation()
+  let place = ''
+  const toggleDrawer = () => setIsOpen(!isOpen)
+
+  if (location.pathname === '/' || location.pathname === '/home') {
+    place = 'home'
+  }
+
+  return (
+    <>
+      <header
+        className={s.header}
+        style={
+          {
+            backgroundColor: place === 'home' ? 'var(--accent)' : 'inherit'
+          } as React.CSSProperties
+        }
+      >
+        <Logo place={place} />
+        <button aria-label="drawer open" onClick={toggleDrawer}>
+          <Icon
+            name="burger"
+            size="32px"
+            color={place === 'home' ? 'var(--text-secondary)' : 'var(--accent)'}
+          />
+        </button>
+      </header>
+
+      <Drawer isOpen={isOpen} onClose={toggleDrawer}>
+        <NavigationLinks onClose={toggleDrawer} />
+        <AuthenticationLinks onClose={toggleDrawer} />
+      </Drawer>
+    </>
+  )
 }
 
 export default Header
