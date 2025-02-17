95a8d4cc1869c922a6829faa0aa5356d82b3b580
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-17 10:41:50 +0200

    create Promo Banners

---
+++ src/components/PromoBanners/PromoBanners.tsx
@@ -0,0 +1,39 @@
+import s from './PromoBanners.module.scss'
+
+const banners = [
+  { title: 'Huge Sale', discount: '70', btnName: 'Shop now' },
+  {
+    title: 'Secure delivery',
+    discount: '100',
+    btnName: 'Read more'
+  },
+  { title: 'Off', discount: '35', btnName: 'Shop now' }
+]
+
+function PromoBanners() {
+  return (
+    <div className={s.promoBanners}>
+      <ul className={s.list}>
+        {banners.map((e, i) => (
+          <li className={s.item} key={i}>
+            <div className={s.box}>
+              <div className={s.number}>{i + 1}</div>
+              <h3 className={s.title}>{e.title}</h3>
+            </div>
+            <div className={s.box}>
+              <span className={s.discount}>{e.discount}%</span>
+              <button
+                className={s.btn}
+                onClick={() => console.log(`${e.discount}`)}
+              >
+                {e.btnName}
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default PromoBanners
