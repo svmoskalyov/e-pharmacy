76dfde97df645f8adbd3a533983e1ed44d632349
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-20 11:58:07 +0200

    modifying Avatar

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -2,10 +2,21 @@ import s from './Avatar.module.scss'
 
 interface AvatarProps {
   name: string
+  size?: string
+  fs?: string
 }
 
-function Avatar({ name }: AvatarProps) {
-  return <div className={s.avatar}>{name[0]}</div>
+function Avatar({ name, size = '40', fs = '14' }: AvatarProps) {
+  return (
+    <div
+      className={s.avatar}
+      style={{ height: `${size}px`, width: `${size}px` }}
+    >
+      <span className={s.name} style={{ fontSize: `${fs}px` }}>
+        {name[0]}
+      </span>
+    </div>
+  )
 }
 
 export default Avatar
