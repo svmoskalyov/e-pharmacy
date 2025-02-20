4a3fcd3b4a433ca6639420cbbd1643f0e7397532
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-20 11:59:03 +0200

    upd Home Page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,20 @@
+import MainBanner from '../../components/MainBanner'
+import NearestMedicineStore from '../../components/NearestMedicineStore'
+import PharmacyPromo from '../../components/PharmacyPromo/PharmacyPromo'
+import PromoBanners from '../../components/PromoBanners'
+import Reviews from '../../components/Reviews'
 import s from './HomePage.module.scss'
 
 function HomePage() {
-  return <div className={s.homePage}>HomePage</div>
+  return (
+    <div className={s.homePage}>
+      <MainBanner />
+      <PromoBanners />
+      <NearestMedicineStore />
+      <PharmacyPromo />
+      <Reviews />
+    </div>
+  )
 }
 
 export default HomePage
