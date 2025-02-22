21b32415bccf1e301f3fd5823fbcaef95fa667e0
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-22 14:51:03 +0200

    !!! draft Product Filter

--- src/components/ProductsFilter/ProductsFilter.tsx
+++ src/components/ProductsFilter/ProductsFilter.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import s from './ProductsFilter.module.scss'
+import Button from '../ui/Button'
+import Icon from '../ui/Icon'
 
 interface Product {
   id: string
@@ -11,72 +13,86 @@ interface Product {
   category: string
 }
 
-const products: Product[] = [
-  {
-    id: '0',
-    photo: 'https://i.ibb.co/bLKP624/5-15-1000x1000-min.jpg',
-    name: 'Aspirin',
-    suppliers: 'Square',
-    stock: '12',
-    price: '89.66',
-    category: 'Medicine'
-  },
-  {
-    id: '1',
-    photo: 'https://i.ibb.co/Hg0zZkQ/shop-4-7-1000x1000-min.jpg',
-    name: 'Paracetamol',
-    suppliers: 'Acme',
-    stock: '19',
-    price: '34.16',
-    category: 'Heart'
-  },
-  {
-    id: '2',
-    photo: 'https://i.ibb.co/02WmJdc/5-19-1000x1000-min.jpg',
-    name: 'Ibuprofen',
-    suppliers: 'Beximco',
-    stock: '09',
-    price: '53.76',
-    category: 'Head'
-  },
-  {
-    id: '3',
-    photo: 'https://i.ibb.co/GxTVSVk/shop-4-9-1000x1000-min.jpg',
-    name: 'Acetaminophen',
-    suppliers: 'ACI',
-    stock: '14',
-    price: '28.57',
-    category: 'Hand'
-  },
-  {
-    id: '4',
-    photo: 'https://i.ibb.co/X330FTj/shop-4-10-1000x1000-min.jpg',
-    name: 'Naproxen',
-    suppliers: 'Uniliver',
-    stock: '10',
-    price: '56.34',
-    category: 'Leg'
-  }
-]
+// const products: Product[] = [
+//   {
+//     id: '0',
+//     photo: 'https://i.ibb.co/bLKP624/5-15-1000x1000-min.jpg',
+//     name: 'Aspirin',
+//     suppliers: 'Square',
+//     stock: '12',
+//     price: '89.66',
+//     category: 'Medicine'
+//   },
+//   {
+//     id: '1',
+//     photo: 'https://i.ibb.co/Hg0zZkQ/shop-4-7-1000x1000-min.jpg',
+//     name: 'Paracetamol',
+//     suppliers: 'Acme',
+//     stock: '19',
+//     price: '34.16',
+//     category: 'Heart'
+//   },
+//   {
+//     id: '2',
+//     photo: 'https://i.ibb.co/02WmJdc/5-19-1000x1000-min.jpg',
+//     name: 'Ibuprofen',
+//     suppliers: 'Beximco',
+//     stock: '09',
+//     price: '53.76',
+//     category: 'Head'
+//   },
+//   {
+//     id: '3',
+//     photo: 'https://i.ibb.co/GxTVSVk/shop-4-9-1000x1000-min.jpg',
+//     name: 'Acetaminophen',
+//     suppliers: 'ACI',
+//     stock: '14',
+//     price: '28.57',
+//     category: 'Hand'
+//   },
+//   {
+//     id: '4',
+//     photo: 'https://i.ibb.co/X330FTj/shop-4-10-1000x1000-min.jpg',
+//     name: 'Naproxen',
+//     suppliers: 'Uniliver',
+//     stock: '10',
+//     price: '56.34',
+//     category: 'Leg'
+//   }
+// ]
 
-function ProductsFilter() {
+interface ProductsFilterProps {
+  products: Product[]
+  filterChoice: (filter: { category: string; search: string }) => void
+}
+
+function ProductsFilter({ products, filterChoice }: ProductsFilterProps) {
   const [selectedCategory, setSelectedCategory] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+  // console.log(selectedCategory, searchQuery)
 
-  const filteredProducts = products.filter(product => {
-    const categoryMatch =
-      selectedCategory === '' || product.category === selectedCategory
-    const nameMatch = product.name
-      .toLowerCase()
-      .includes(searchQuery.toLowerCase())
-    return categoryMatch && nameMatch
-  })
+  // const filteredProducts = products.filter(product => {
+  //   const categoryMatch =
+  //     selectedCategory === '' || product.category === selectedCategory
+  //   const nameMatch = product.name
+  //     .toLowerCase()
+  //     .includes(searchQuery.toLowerCase())
+  //   return categoryMatch && nameMatch
+  // })
+
+  // console.log('filteredProducts --', filteredProducts)
+
+  const sendFilterChoice = () => {
+    console.log('filter send --', selectedCategory, searchQuery)
+    filterChoice({ category: selectedCategory, search: searchQuery })
+  }
 
   return (
     <div className={s.productFilterContainer}>
       <div className={s.filterControls}>
-        <label htmlFor="category">Product category:</label>
+        {/* <label htmlFor="category">Product category:</label> */}
         <select
+          className={s.select}
           id="category"
           value={selectedCategory}
           onChange={e => setSelectedCategory(e.target.value)}
@@ -91,16 +107,30 @@ function ProductsFilter() {
           )}
         </select>
 
-        <label htmlFor="search">Search medicine:</label>
+        {/* <label htmlFor="search">Search medicine:</label> */}
         <input
+          className={s.searchInput}
           type="text"
           id="search"
+          placeholder="qwdwq"
           value={searchQuery}
           onChange={e => setSearchQuery(e.target.value)}
         />
       </div>
 
-      <ul className={s.productList}>
+      <Button
+        onClick={sendFilterChoice}
+        icon={<Icon name="filter" size="14" />}
+        style={{ width: '116px' }}
+      >
+        <span className={s.btnText}>Filter</span>
+      </Button>
+
+      {/* <button type="button" onClick={sendFilterChoice}>
+        filter
+      </button> */}
+
+      {/* <ul className={s.productList}>
         {filteredProducts.map(product => (
           <li className={s.productImage} key={product.id}>
             <img
@@ -117,7 +147,7 @@ function ProductsFilter() {
             </div>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
