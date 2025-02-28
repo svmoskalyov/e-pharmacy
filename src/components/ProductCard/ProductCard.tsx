84ca10b3642fe657bae3d4ca38e7f3479acef7e0
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-02-28 11:35:03 +0200

    upd ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
+import { useLocation } from 'react-router'
 import Button from '../ui/Button'
 import ButtonLink from '../ui/ButtonLink'
 import s from './ProductCard.module.scss'
+import ProductQuantity from '../ProductQuantity'
 
 interface ProductProps {
   id: string
@@ -8,13 +11,36 @@ interface ProductProps {
   name: string
   suppliers: string
   price: string
+  stock: string
 }
 
-function ProductCard({ id, photo, name, suppliers, price }: ProductProps) {
+function ProductCard({
+  id,
+  photo,
+  name,
+  suppliers,
+  price,
+  stock
+}: ProductProps) {
+  const { pathname } = useLocation()
+  // const [prodQuantity, setProdQuantity] = useState<number>(parseInt(stock, 10))
+  const [prodInCart, setProdInCart] = useState(0)
+
+  const place = pathname === '/medicine' ? true : false
+  // console.log(place)
+
+  const addToCart = () => {
+    console.log('add to cart id --', id)
+    console.log('state in stock --', stock)
+    console.log('add to cart prodInCart --', prodInCart)
+    console.log('change in stock --', parseInt(stock, 10) - prodInCart)
+  }
+
   return (
     <div className={s.productCard}>
       <img className={s.image} src={photo} alt={name} />
-      <ul className={s.details}>
+
+      <ul className={`${s.details} ${place ? s['med'] : s['prod']}`}>
         <li className={s.item}>
           <h3 className={s.name}>{name}</h3>
           <p className={s.price}>
@@ -26,20 +52,28 @@ function ProductCard({ id, photo, name, suppliers, price }: ProductProps) {
           <p className={s.suppliers}>{suppliers}</p>
         </li>
         <li className={s.item}>
+          {!place && (
+            <ProductQuantity
+              onQuantityChange={quantity => setProdInCart(quantity)}
+            />
+          )}
+
           <Button
             variant="contained"
-            size="small"
-            onClick={() => console.log('add to cart')}
+            size={place ? 'small' : 'medium'}
+            onClick={addToCart}
           >
             Add to cart
           </Button>
-          <ButtonLink
-            to={`/product/${id}`}
-            variant="text"
-            style={{ color: 'var(--text-primary)' }}
-          >
-            Details
-          </ButtonLink>
+          {place && (
+            <ButtonLink
+              to={`/product/${id}`}
+              variant="text"
+              style={{ color: 'var(--text-primary)' }}
+            >
+              Details
+            </ButtonLink>
+          )}
         </li>
       </ul>
     </div>
