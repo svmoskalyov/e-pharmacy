a9c5699e63c9b7d2393eadf044f07921d7198d2b
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-01 10:15:58 +0200

    create RadioButton

---
+++ src/components/ui/RadioButton/RadioButton.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import s from './RadioButton.module.scss'
+
+interface RadioButtonProps {
+  options: string[]
+  onChange: (value: string) => void
+}
+
+function RadioButton({ options, onChange }: RadioButtonProps) {
+  const [selectedValue, setSelectedValue] = useState<string>(options[0])
+
+  const handleOptionChange = (value: string) => {
+    setSelectedValue(value)
+    onChange(value)
+  }
+
+  return (
+    <div className={s.radioGroup}>
+      {options.map(option => (
+        <label key={option} className={s.radioLabel}>
+          <input
+            type="radio"
+            value={option}
+            checked={selectedValue === option}
+            onChange={() => handleOptionChange(option)}
+            className={s.radioInput}
+          />
+          <span className={s.radioText}>{option}</span>
+        </label>
+      ))}
+    </div>
+  )
+}
+
+export default RadioButton
