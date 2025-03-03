0eee14f9ce6ad73a70206914bb22fb75ab7e1db9
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-03 16:18:45 +0200

    create RegisterPage

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,23 @@
+import RegisterForm from '../../components/RegisterForm'
+import DecorLines from '../../components/ui/DecorLines'
 import s from './RegisterPage.module.scss'
 
 function RegisterPage() {
-  return <div className={s.registerPage}>RegisterPage</div>
+  return (
+    <div className={s.registerPage}>
+      <div className={s.headerBox}>
+        <h2 className={s.title}>
+          Your medication, delivered Say goodbye to all{' '}
+          <span className={s.mark}> your healthcare </span> worries with us
+        </h2>
+        <div className={s.img}></div>
+      </div>
+      <RegisterForm />
+      <div className={s.decor}>
+        <DecorLines variant="auth" />
+      </div>
+    </div>
+  )
 }
 
 export default RegisterPage
