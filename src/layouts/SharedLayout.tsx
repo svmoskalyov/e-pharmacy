0dab862a88600a531659567c7893c5a4021783cb
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-03 08:59:30 +0200

    upd SharedLayout

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -1,19 +1,27 @@
-import { Suspense } from 'react'
-import { Outlet } from 'react-router'
+import { CSSProperties, Suspense } from 'react'
+import { Outlet, useLocation } from 'react-router'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import s from './SharedLayout.module.scss'
 
 function SharedLayout() {
+  const { pathname } = useLocation()
+  const place = pathname === '/register' || pathname === '/login' ? true : false
+
   return (
-    <div className={s.sharedLayout}>
+    <div
+      className={s.sharedLayout}
+      style={
+        { '--footer': `"footer" ${place ? '0' : '334px'}` } as CSSProperties
+      }
+    >
       <Header />
       <main className={s.main}>
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
       </main>
-      <Footer />
+      {!place && <Footer />}
     </div>
   )
 }
