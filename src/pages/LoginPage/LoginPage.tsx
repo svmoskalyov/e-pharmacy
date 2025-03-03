77eeb722ff65c291613887e86d031103a7d997a2
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-03 16:18:57 +0200

    create LoginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,23 @@
+import LoginForm from '../../components/LoginForm'
+import DecorLines from '../../components/ui/DecorLines'
 import s from './LoginPage.module.scss'
 
 function LoginPage() {
-  return <div className={s.loginPage}>LoginPage</div>
+  return (
+    <div className={s.loginPage}>
+      <div className={s.headerBox}>
+        <h2 className={s.title}>
+          Your medication, delivered Say goodbye to all{' '}
+          <span className={s.mark}> your healthcare </span> worries with us
+        </h2>
+        <div className={s.img}></div>
+      </div>
+      <LoginForm />
+      <div className={s.decor}>
+        <DecorLines variant="auth" />
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
