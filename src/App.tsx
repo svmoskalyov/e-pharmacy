b47223d171ff7111a182a6795f4779818917430a
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-03 08:58:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { lazy } from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 import SharedLayout from './layouts/SharedLayout'
 import HomePage from './pages/HomePage'
-import RegisterPage from './pages/RegisterPage'
-import LoginPage from './pages/LoginPage'
-import MedicineStorePage from './pages/MedicineStorePage'
-import MedicinePage from './pages/MedicinePage'
-import ProductPage from './pages/ProductPage'
-import CartPage from './pages/CartPage'
+
+const RegisterPage = lazy(() => import('./pages/RegisterPage'))
+const LoginPage = lazy(() => import('./pages/LoginPage'))
+const MedicineStorePage = lazy(() => import('./pages/MedicineStorePage'))
+const MedicinePage = lazy(() => import('./pages/MedicinePage'))
+const ProductPage = lazy(() => import('./pages/ProductPage'))
+const CartPage = lazy(() => import('./pages/CartPage'))
 
 function App() {
+  const isAuth = false
+
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
@@ -17,9 +21,9 @@ function App() {
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="medicine-store" element={<MedicineStorePage />} />
-        <Route path="medicine" element={<MedicinePage />} />
         <Route path="product/:prodId" element={<ProductPage />} />
-        <Route path="cart" element={<CartPage />} />
+        {isAuth && <Route path="medicine" element={<MedicinePage />} />}
+        {isAuth && <Route path="cart" element={<CartPage />} />}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
