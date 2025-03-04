eff6a2ad4c1f12c4d4277334e94d531da7733001
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-04 15:16:51 +0200

    create AuthPopupProps

---
+++ src/components/AuthPopup/AuthPopup.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import LoginForm from '../LoginForm'
+import RegisterForm from '../RegisterForm'
+import Icon from '../ui/Icon'
+import s from './AuthPopup.module.scss'
+
+interface AuthPopupProps {
+  onClose: () => void
+}
+
+function AuthPopup({ onClose }: AuthPopupProps) {
+  const [toggleAuth, setToggleAuth] = useState(true)
+
+  const handleToggleAuth = () => {
+    setToggleAuth(!toggleAuth)
+  }
+
+  return (
+    <div className={s.authPopup}>
+      <h2 className={s.title}>
+        {toggleAuth ? 'Sign Up' : 'Log in to your account'}
+      </h2>
+      <p className={s.subtitle}>
+        {toggleAuth
+          ? 'Before proceeding, please register on our site.'
+          : 'Please login to your account before continuing.'}
+      </p>
+      {toggleAuth && (
+        <>
+          <RegisterForm />
+          <button className={s.btnToggle} onClick={handleToggleAuth}>
+            Already have an account?
+          </button>
+        </>
+      )}
+      {!toggleAuth && (
+        <>
+          <LoginForm popupAuth />
+          <button className={s.btnToggle} onClick={handleToggleAuth}>
+            Don't have an account?
+          </button>
+        </>
+      )}
+      <button className={s.btnClose} aria-label="close popup" onClick={onClose}>
+        <Icon name="close" size="20px" color="#000" sw="4" />
+      </button>
+    </div>
+  )
+}
+
+export default AuthPopup
