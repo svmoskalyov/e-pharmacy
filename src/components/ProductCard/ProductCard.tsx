0dd62593b529c354197e5f3bea10f83ac44805b0
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-04 15:17:06 +0200

    Update ProductCard.tsx

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -4,6 +4,8 @@ import Button from '../ui/Button'
 import ButtonLink from '../ui/ButtonLink'
 import s from './ProductCard.module.scss'
 import ProductQuantity from '../ProductQuantity'
+import Backdrop from '../Backdrop'
+import AuthPopup from '../AuthPopup/AuthPopup'
 
 interface ProductProps {
   id: string
@@ -25,58 +27,78 @@ function ProductCard({
   const { pathname } = useLocation()
   // const [prodQuantity, setProdQuantity] = useState<number>(parseInt(stock, 10))
   const [prodInCart, setProdInCart] = useState(0)
+  const [showBackdrop, setShowBackdrop] = useState(false)
+  const isAuth = false
 
   const place = pathname === '/medicine' ? true : false
   // console.log(place)
+
+  const handleBackdropClick = () => {
+    setShowBackdrop(false)
+  }
 
   const addToCart = () => {
     console.log('add to cart id --', id)
     console.log('state in stock --', stock)
     console.log('add to cart prodInCart --', prodInCart)
     console.log('change in stock --', parseInt(stock, 10) - prodInCart)
+
+    if (!isAuth) {
+      console.log('if isAuth --', isAuth)
+      setShowBackdrop(true)
+    }
   }
 
   return (
-    <div className={s.productCard}>
-      <img className={s.image} src={photo} alt={name} />
+    <>
+      <div className={s.productCard}>
+        <img className={s.image} src={photo} alt={name} />
 
-      <ul className={`${s.details} ${place ? s['med'] : s['prod']}`}>
-        <li className={s.item}>
-          <h3 className={s.name}>{name}</h3>
-          <p className={s.price}>
-            <span className={s.rupee}>&#x9F3;</span>
-            {price}
-          </p>
-        </li>
-        <li className={s.item}>
-          <p className={s.suppliers}>{suppliers}</p>
-        </li>
-        <li className={s.item}>
-          {!place && (
-            <ProductQuantity
-              onQuantityChange={quantity => setProdInCart(quantity)}
-            />
-          )}
+        <ul className={`${s.details} ${place ? s['med'] : s['prod']}`}>
+          <li className={s.item}>
+            <h3 className={s.name}>{name}</h3>
+            <p className={s.price}>
+              <span className={s.rupee}>&#x9F3;</span>
+              {price}
+            </p>
+          </li>
+          <li className={s.item}>
+            <p className={s.suppliers}>{suppliers}</p>
+          </li>
+          <li className={s.item}>
+            {!place && (
+              <ProductQuantity
+                onQuantityChange={quantity => setProdInCart(quantity)}
+              />
+            )}
 
-          <Button
-            variant="contained"
-            size={place ? 'small' : 'medium'}
-            onClick={addToCart}
-          >
-            Add to cart
-          </Button>
-          {place && (
-            <ButtonLink
-              to={`/product/${id}`}
-              variant="text"
-              style={{ color: 'var(--text-primary)' }}
+            <Button
+              variant="contained"
+              size={place ? 'small' : 'medium'}
+              onClick={addToCart}
             >
-              Details
-            </ButtonLink>
-          )}
-        </li>
-      </ul>
-    </div>
+              Add to cart
+            </Button>
+            {place && (
+              <ButtonLink
+                to={`/product/${id}`}
+                variant="text"
+                style={{ color: 'var(--text-primary)' }}
+              >
+                Details
+              </ButtonLink>
+            )}
+          </li>
+        </ul>
+      </div>
+      <Backdrop
+        show={showBackdrop}
+        onClick={handleBackdropClick}
+        disableClick={false}
+      >
+        <AuthPopup onClose={handleBackdropClick} />
+      </Backdrop>
+    </>
   )
 }
 
