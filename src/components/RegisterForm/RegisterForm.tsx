51e2309ff314fe5d4ef4527553ebc35f633d153b
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-04 15:17:27 +0200

    upd RegisterForm

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,7 +1,74 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import Button from '../ui/Button'
+import Input from '../ui/Input'
 import s from './RegisterForm.module.scss'
 
 function RegisterForm() {
-  return <div className={s.registerForm}>RegisterForm</div>
+  const [error, setError] = useState('')
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    password: ''
+  })
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    console.log('Data form:', formData)
+    if (formData.name === '') setError('name is not empty')
+  }
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <div className={s.inputsBox}>
+        <Input
+          type="text"
+          name="name"
+          placeholder="User Name"
+          placeholdercolor="#1d1e2166"
+          value={formData.name}
+          onChange={handleInputChange}
+          error={error}
+        />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email address"
+          placeholdercolor="#1d1e2166"
+          value={formData.email}
+          onChange={handleInputChange}
+          error={error}
+        />
+        <Input
+          type="tel"
+          name="phone"
+          placeholder="Phone number"
+          placeholdercolor="#1d1e2166"
+          value={formData.phone}
+          onChange={handleInputChange}
+          error={error}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          placeholdercolor="#1d1e2166"
+          value={formData.password}
+          onChange={handleInputChange}
+          error={error}
+        />
+      </div>
+      <Button type="submit">Register</Button>
+    </form>
+  )
 }
 
 export default RegisterForm
