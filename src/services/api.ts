5b3b98503e27f1dba70e5e6d7911681fc75867b2
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-05 15:22:39 +0200

    Create api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,67 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  UserCredential
+} from 'firebase/auth'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
+import './firebaseConfig'
+import { useAuthStore } from '../stores/authStore'
+
+const auth = getAuth()
+const db = getFirestore()
+
+interface RegistrationData {
+  name: string
+  email: string
+  password: string
+  phoneNumber: string
+}
+
+interface RegistrationResult {
+  success: boolean
+  message: string
+}
+
+export const registerUser = async (
+  data: RegistrationData
+): Promise<RegistrationResult> => {
+  const { setIsLoading, setError, setUser, setIsAuth } = useAuthStore.getState()
+  setIsLoading(true)
+  setError(null)
+
+  try {
+    const userCredential: UserCredential = await createUserWithEmailAndPassword(
+      auth,
+      data.email,
+      data.password
+    )
+    const user = userCredential.user
+
+    await setDoc(doc(db, 'users', user.uid), {
+      name: data.name,
+      email: data.email,
+      phoneNumber: data.phoneNumber
+    })
+
+    const userData = {
+      uid: user.uid,
+      name: data.name,
+      email: data.email,
+      phoneNumber: data.phoneNumber,
+      password: data.password
+    }
+    console.log('userData --', userData)
+
+    setUser(userData)
+    setIsAuth(true)
+    setIsLoading(false)
+    return { success: true, message: 'Реєстрація успішна!' }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      setError(error.message)
+      setIsLoading(false)
+      return { success: false, message: error.message }
+    }
+    return { success: false, message: 'An unknown error occurred' }
+  }
+}
