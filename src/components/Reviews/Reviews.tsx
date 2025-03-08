7699c09961d3b78307c1cf74af0ab37dffbde00b
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-08 12:57:37 +0200

    Update Reviews.tsx

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,24 +1,8 @@
+import { useEffect } from 'react'
+import { useReviewStore } from '../../stores/reviewStore'
 import SectionWrapper from '../SectionWrapper'
 import Avatar from '../ui/Avatar'
 import s from './Reviews.module.scss'
-
-const reviews: Review[] = [
-  {
-    name: 'Maria Tkachuk',
-    testimonial:
-      'I recently used this medical platform to book an appointment with a specialist, and I was impressed by how easy and user-friendly the process was. Highly recommended!'
-  },
-  {
-    name: 'Sergey Rybachok',
-    testimonial:
-      'I had a great experience using this medical platform to access my health records. This platform is a game-changer for managing my healthcare needs.'
-  },
-  {
-    name: 'Natalia Chatuk',
-    testimonial:
-      'I recently had a virtual appointment with my doctor through this medical platform, and I was pleasantly surprised by how seamless the experience was.'
-  }
-]
 
 type Review = {
   name: string
@@ -26,11 +10,19 @@ type Review = {
 }
 
 function Reviews() {
+  const reviews = useReviewStore(state => state.reviews)
+  const fetchReviews = useReviewStore(state => state.fetchReviews)
+
   const getRandomObjects = (arr: Review[], count: number): Review[] => {
     const shuffled = [...arr].sort(() => Math.random() - 0.5)
     return shuffled.slice(0, count)
   }
+
   const randomObjects = getRandomObjects(reviews, 1)
+
+  useEffect(() => {
+    if (reviews.length === 0) fetchReviews()
+  }, [fetchReviews, reviews.length])
 
   return (
     <SectionWrapper>
@@ -45,7 +37,7 @@ function Reviews() {
           {randomObjects.map((e, i) => (
             <div key={i} className={s.item}>
               <div className={s.avatarWrapper}>
-                <Avatar name={e.name} size='64' fs='32'/>
+                <Avatar name={e.name} size="64" fs="32" />
               </div>
               <h3 className={s.name}>{e.name}</h3>
               <p className={s.testimonial}>{e.testimonial}</p>
