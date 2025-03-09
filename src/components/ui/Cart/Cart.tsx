53877d334558311dd0ff656560b33de5c6ec7cdd
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-09 14:40:00 +0200

    Update Cart.tsx

--- src/components/ui/Cart/Cart.tsx
+++ src/components/ui/Cart/Cart.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router'
 import Icon from '../Icon'
 import s from './Cart.module.scss'
 
@@ -7,10 +8,10 @@ interface CartProps {
 
 function Cart({ count }: CartProps) {
   return (
-    <div className={s.cart}>
-      <Icon name='cart' size='16' />
+    <NavLink to="cart" className={s.cart}>
+      <Icon name="cart" size="16" />
       <span className={s.badge}>{count}</span>
-    </div>
+    </NavLink>
   )
 }
 
