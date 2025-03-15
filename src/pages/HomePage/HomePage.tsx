6c751134c833e8972f845972a0dcb432e9a6df78
Serhii Moskalov <s.v.moskalyov+github.com@gmail.com>
2025-03-15 13:22:32 +0200

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,17 +3,16 @@ import NearestMedicineStore from '../../components/NearestMedicineStore'
 import PharmacyPromo from '../../components/PharmacyPromo/PharmacyPromo'
 import PromoBanners from '../../components/PromoBanners'
 import Reviews from '../../components/Reviews'
-import s from './HomePage.module.scss'
 
 function HomePage() {
   return (
-    <div className={s.homePage}>
+    <>
       <MainBanner />
       <PromoBanners />
       <NearestMedicineStore />
       <PharmacyPromo />
       <Reviews />
-    </div>
+    </>
   )
 }
 
